5a1f6a294b5fad7ab574c83924434107ad53337e
cyp <martinfischer533@gmail.com>
2026-06-02 00:07:14 +0200

    remove modules (are seperate now)

--- RoosterLocalization/src/main/kotlin/dev/cypdashuhn/rooster/localization/Minimessages.kt
+++
@@ -1,6 +0,0 @@
-package dev.cypdashuhn.rooster.localization
-
-import net.kyori.adventure.text.TextComponent
-import net.kyori.adventure.text.minimessage.MiniMessage
-
-fun minimessage(text: String) = MiniMessage.miniMessage().deserialize(text) as TextComponent
