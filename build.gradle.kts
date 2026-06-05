6493e34fcb6cc1849a5c68162064030ee70bb185
cyp <martinfischer533@gmail.com>
2026-06-05 02:27:22 +0200

    remove localization coupling

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     testImplementation("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 
     implementation("dev.rooster.core:rooster-core:1.0-SNAPSHOT")
-    implementation("dev.rooster.localization:rooster-localization:1.0-SNAPSHOT")
 }
 
 tasks.test {
